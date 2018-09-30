01393d151e39954f4554d2cf52fe7e516266b01e
Me Not Sure Who Can See This <andoni.oc@gmail.com>
2018-09-30 21:46:40 +0100

    Twitter part commented out but now running correctly as a Lambda in AWS
    Also not using the 'Yargs' command line argument functionality.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const argv = yargs
       demand: false,
       alias: 'limit',
       describe: 'Number of blog entries to retrieve.',
-      default: 1,
+      default: 2,
       string: true // make sure an address is there!
     }
   })
@@ -17,15 +17,19 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-awsblog.getBlogPost('SortOrderValue', false, argv.limit, 'en_US', (error, blogPosts) => {
-  if(error) {
-    console.log('Oops! There was an error: ' + error);
-  }
-  else {
-  //  console.log(JSON.stringify(blogPosts, undefined, 4));
-    for(var i = 0; i < blogPosts.items.length; i++) {
-      console.log(blogPosts.items[i].id + ') ' + blogPosts.items[i].additionalFields.link);
-      twitter.sendTweet(blogPosts.items[i].additionalFields.link);
+// This wrapper is required by AWS Lambda
+exports.sendTweets = function (event, context, callback) {
+  console.log('Beginning sendTweets() function: ');
+  awsblog.getBlogPost('SortOrderValue', false, 5, 'en_US', (error, blogPosts) => {
+    if(error) {
+      console.log('Oops! There was an error: ' + error);
     }
-  }
-});
+    else {
+    //  console.log(JSON.stringify(blogPosts, undefined, 4));
+      for(var i = 0; i < blogPosts.items.length; i++) {
+        console.log(blogPosts.items[i].id + ') ' + blogPosts.items[i].additionalFields.link);
+        // twitter.sendTweet(blogPosts.items[i].additionalFields.link);
+      }
+    }
+  });
+};
