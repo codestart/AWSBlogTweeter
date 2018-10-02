eb681b62f42c97e14c581a3eb2aa5e3e953890b0
Me Not Sure Who Can See This <andoni.oc@gmail.com>
2018-10-02 22:21:32 +0100

    Minor code fixes
    Moved configuration out to env variables.

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const twitter = require('./twitter/twitter.js');
 
 // This wrapper is required by AWS Lambda
 exports.sendTweets = function (event, context, callback) {
+  var NUMBER_TO_CHECK = process.env.NUMBER_TO_CHECK;
+  var HOURS_IN_PERIOD = process.env.HOURS_IN_PERIOD;
   console.log('Beginning sendTweets() function: ');
-  const postResult = awsblog.getBlogPost('SortOrderValue', false, 5, 'en_US', (error, blogPosts) => {
+  const postResult = awsblog.getBlogPost('SortOrderValue', false, NUMBER_TO_CHECK, 'en_US', (error, blogPosts) => {
     if(error) {
       console.log('Oops! There was an error: ' + error);
       callback(error);
@@ -17,9 +19,11 @@ exports.sendTweets = function (event, context, callback) {
       // Work from the oldest back to 0 (the newest)
       for(var i = blogPosts.items.length - 1; i >= 0; i--) {
         // e.g.:  "2018-09-30T15:46:10+0000"
-        var blogPostTimestamp = dateTime.parse(blogPosts.items[i].dateUpdated, "YYYY-MM-DDTHH:mm:ss+0000");
+        var strTime = new String(blogPosts.items[i].dateUpdated);
+        strTime = strTime.substr(0, "YYYY-MM-DDTHH:mm:ss".length);
+        var blogPostTimestamp = dateTime.parse(strTime, "YYYY-MM-DDTHH:mm:ss");
         var currentTime = new Date();
-        const FOUR_HOURS = 1000 * 60 * 60 * 4;
+        const FOUR_HOURS = 1000 * 60 * 60 * HOURS_IN_PERIOD;
         const BLOG_POST_AGE = currentTime - blogPostTimestamp;
         console.log('Blog Post Age: ' + BLOG_POST_AGE);
         console.log('Age Diff: ' + (BLOG_POST_AGE - FOUR_HOURS));
@@ -28,7 +32,7 @@ exports.sendTweets = function (event, context, callback) {
 
         if(BLOG_POST_AGE < FOUR_HOURS) {
           console.log('** POSTED **');
-//           callback(undefined, blogPosts.items[i].id + ') ' + blogPosts.items[i].additionalFields.link);
+          callback(undefined, blogPosts.items[i].id + ') ' + blogPosts.items[i].additionalFields.link);
           twitter.sendTweet(blogPosts.items[i].additionalFields.link);
         }
 
