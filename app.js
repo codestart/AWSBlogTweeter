f177945024a6903343d0d4b33f606334a7a3d9bd
Me Not Sure Who Can See This <andoni.oc@gmail.com>
2018-10-15 00:06:38 +0100

    Try out moving to promises
    Also moving to DynamoDB for the blog names.
    Promises helpful because of the 3 synchronized calles to: 
    1) Request from the blog
    2) Call the DB
    3) Send info to Twitter
    
    Work-In-Progress! Code runs but does nothing more than print stuff out.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const dateTime = require('date-and-time');
+const axios = require('axios');
 
 const awsblog = require('./awsblog/index.js');
 const dynamo = require('./dynamodb/index.js');
@@ -6,51 +7,61 @@ const twitter = require('./twitter/twitter.js');
 
 // This wrapper is required by AWS Lambda
 exports.sendTweets = function (event, context, callback) {
+  console.log('Beginning sendTweets() function: ');
+
   var NUMBER_TO_CHECK = process.env.NUMBER_TO_CHECK | 3;
   var MINUTES_IN_PERIOD = process.env.MINUTES_IN_PERIOD | 5;
   var BLOG_ADDRESS_STUB = 'https://aws.amazon.com/blogs/';
+  var DATE_FORMAT = 'YYYY-MM-DDTHH:mm:ss';
 
-  console.log('Beginning sendTweets() function: ');
-  const postResult = awsblog.getBlogPost('SortOrderValue', false, NUMBER_TO_CHECK, 'en_US', (error, blogPosts) => {
-    var callbackDB = dynamo.queryDatabase('iot', (error, results) => {
-        // var resultsLocal = results;
-        console.log('BlogSection is: ', results.BlogSection.S, 'Hashtag is: ', results.Hashtag.S);
-    });
+  const orderBy = 'SortOrderValue';
+  const sortAscending = false;
+  const limit = NUMBER_TO_CHECK;
+  const locale = 'en_US';
+  const awsBlogUrl = `https://aws.amazon.com/api/dirs/blog-posts/items?order_by=${orderBy}&sort_ascending=${sortAscending}&limit=${limit}&locale=${locale}`;
 
-    if(error) {
+  //  const postResult = awsblog.getBlogPost('SortOrderValue', false, NUMBER_TO_CHECK, 'en_US', (error, blogPosts) => {
+
+  axios.get(awsBlogUrl).then((response) => {
+    // success case expression:
+    const blogPosts = response.data;
+    //console.log(JSON.stringify(blogPosts, undefined, 4));
+    var event = [];
+    var eventNo = 0;
+    // Work from the oldest back to 0 (the newest)
+    blog_post_items:
+    for(var i = blogPosts.items.length - 1; i >= 0; i--) {
+      // e.g.:  "2018-09-30T15:46:10+0000"
+      var strTime = blogPosts.items[i].dateUpdated;
+      strTime = strTime.substr(0, DATE_FORMAT.length);
+      var blogPostTimestamp = dateTime.parse(strTime, DATE_FORMAT);
+      var currentTime = new Date();
+      const PERIOD = 1000 * 60 * MINUTES_IN_PERIOD;
+      const BLOG_POST_AGE = currentTime - blogPostTimestamp;
+//      console.log('Blog Post Age: ' + BLOG_POST_AGE);
+//      console.log('Age Diff: ' + (BLOG_POST_AGE - PERIOD));
+//      console.log('Blog post timestamp: ' + blogPosts.items[i].dateUpdated);
+//      console.log('Post details: ' + blogPosts.items[i].id + ') ' + blogPosts.items[i].additionalFields.link);
+
+      var strUrl = blogPosts.items[i].additionalFields.link;
+      var changeableUrl = strUrl.substr(BLOG_ADDRESS_STUB.length);
+      var sectionName = changeableUrl.substr(0, changeableUrl.indexOf('/'));
+
+      if(true) { //BLOG_POST_AGE < PERIOD) {
+        for(var x = 0; x < event.length; x++) {
+          if(event[x].URLSection.S === sectionName) continue blog_post_items;
+        }
+        event[eventNo] = {'URLSection':{S:sectionName}};
+        eventNo++;
+      }
+    }
+    return dynamo.queryDatabase(event, undefined);
+  }).then((resolve) => {
+    console.log('2nd in chain Blog: ', JSON.stringify(resolve, undefined, 4));
+    console.log('Pub Sec Blog Name is: ', resolve.body.data['publicsector'][0])
+  }).catch((error) => {
       console.log('Oops! There was an error: ' + error);
       callback(error);
-    }
-    else {
-      //console.log(JSON.stringify(blogPosts, undefined, 4));
-
-      // Work from the oldest back to 0 (the newest)
-      for(var i = blogPosts.items.length - 1; i >= 0; i--) {
-        // e.g.:  "2018-09-30T15:46:10+0000"
-        var strTime = new String(blogPosts.items[i].dateUpdated);
-        strTime = strTime.substr(0, "YYYY-MM-DDTHH:mm:ss".length);
-        var blogPostTimestamp = dateTime.parse(strTime, "YYYY-MM-DDTHH:mm:ss");
-        var currentTime = new Date();
-        const PERIOD = 1000 * 60 * MINUTES_IN_PERIOD;
-        const BLOG_POST_AGE = currentTime - blogPostTimestamp;
-        console.log('Blog Post Age: ' + BLOG_POST_AGE);
-        console.log('Age Diff: ' + (BLOG_POST_AGE - PERIOD));
-        console.log('Blog post timestamp: ' + blogPosts.items[i].dateUpdated);
-        console.log('Post details: ' + blogPosts.items[i].id + ') ' + blogPosts.items[i].additionalFields.link);
-
-        var strUrl = new String(blogPosts.items[i].additionalFields.link);
-        var changeableUrl = strUrl.substr(BLOG_ADDRESS_STUB.length);
-        var sectionName = changeableUrl.substr(0, changeableUrl.indexOf('/'));
-        console.log('Section Name: ', sectionName);
-        console.log('Section Title: ', awsblog.awsBlogSections[sectionName]);
-
-        if(BLOG_POST_AGE < PERIOD) {
-          console.log('** POSTED **');
-//          twitter.sendTweet('The AWS ' + awsblog.awsBlogSections[sectionName] + ' Blog #' + awsblog.awsBlogHashtags[sectionName] + ' ' + blogPosts.items[i].additionalFields.link);
-          console.log(awsblog.awsBlogSections[sectionName] + ' #' + awsblog.awsBlogHashtags[sectionName] + ' ' + blogPosts.items[i].additionalFields.link);
-        }
-      }
-      callback(undefined, 'No Return Value Needed ;-)');
-    }
   });
-};
+  callback(undefined, 'No Return Value Needed ;-)');
+}
--- dynamodb/index.js
+++ dynamodb/index.js
@@ -1,38 +1,44 @@
 // Load the AWS SDK for Node.js
 var AWS = require('aws-sdk');
 
-var queryDatabase = (urlsection, callback) => {
-  // Set the region
-  AWS.config.update({region: 'eu-west-1'});
+// Set the region
+AWS.config.update({region: 'eu-west-1'});
 
-  // Create DynamoDB service object
-  var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+// Create DynamoDB service object
+var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+var queryDatabase = async (event, context) => {
+//  console.log('Event is: ', JSON.stringify(event, undefined, 4));
 
   var params = {
-    ExpressionAttributeValues: {
-      ':s': {S: urlsection}
-  //    ,
-  //    ':e' : {N: '09'},
-  //    ':topic' : {S: 'PHRASE'}
-     },
-   KeyConditionExpression: 'URLSection = :s',
-   ProjectionExpression: 'BlogSection, Hashtag',
-  //  FilterExpression: 'contains (Subtitle, :topic)',
-   TableName: 'AWS_BLOGS'
+    RequestItems: {
+      'AWS_BLOGS': {
+        Keys: event,
+        ProjectionExpression: 'URLSection, BlogSection, Hashtag'
+      }
+    }
   };
 
-  ddb.query(params, function(err, data) {
-    if (err) {
-      console.log("Error", err);
-      callback(err);
-    } else {
-      data.Items.forEach(function(element, index, array) {
-        console.log(element.BlogSection.S + " (" + element.Hashtag.S + ")");
-        callback(undefined, element);
-      });
-    }
-  });
-}
+  try {
+    var data = await ddb.batchGetItem(params).promise();
+    var data = reArrangeEntries(data);
+    return { statusCode: 200, body: { params, data } };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      error: `Could not post: ${error.stack}`
+    };
+  }
+};
+
+var reArrangeEntries = (data) => {
+  var resultsArr = data.Responses['AWS_BLOGS'];
+  var output = {};
+  for(var result of resultsArr) {
+    output[result.URLSection.S] = [result.BlogSection.S, result.Hashtag.S];
+  }
+  return output;
+};
 
 module.exports = {
     queryDatabase: queryDatabase
