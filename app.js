ea7cf8fe8b7b6727d6d3525055a83a298df522ff
Me Not Sure Who Can See This <andoni.oc@gmail.com>
2019-04-30 01:13:10 +0100

    Updates towards moving away from time

--- app.js
+++ app.js
@@ -5,113 +5,118 @@ const twitter = require('./twitter/twitter.js');
 
 // This wrapper is required by AWS Lambda
 exports.sendTweets = function (event, context, callback) {
-  var NUMBER_TO_CHECK = process.env.NUMBER_TO_CHECK;
-  var MINUTES_IN_PERIOD = process.env.MINUTES_IN_PERIOD;
-  var TWITTER_ON = (process.env.TWITTER_ON.toLowerCase().trim() === 'true');
-  var TWITTER_ACCOUNT = process.env.TWITTER_ACCOUNT;
-  const ENV = process.env.ENV;
+    var NUMBER_TO_CHECK = process.env.NUMBER_TO_CHECK;
+    var MINUTES_IN_PERIOD = process.env.MINUTES_IN_PERIOD;
+    var TWITTER_ON = (process.env.TWITTER_ON.toLowerCase().trim() === 'true');
+    var TWITTER_ACCOUNT = process.env.TWITTER_ACCOUNT;
+    const ENV = process.env.ENV;
 
-  var BLOG_ADDRESS_STUB = 'https://aws.amazon.com/blogs/';
-  var DATE_FORMAT = 'YYYY-MM-DDTHH:mm:ss';
+    var BLOG_ADDRESS_STUB = 'https://aws.amazon.com/blogs/';
+    var DATE_FORMAT = 'YYYY-MM-DDTHH:mm:ss';
 
-  const orderBy = 'SortOrderValue';
-  const sortAscending = false;
-  const limit = NUMBER_TO_CHECK;
-  const locale = 'en_US';
-  const awsBlogUrl =
-    `https://aws.amazon.com/api/dirs/blog-posts/items?` +
-    `order_by=${orderBy}` +
-    `&sort_ascending=${sortAscending}` +
-    `&limit=${limit}` +
-    `&locale=${locale}`;
-  // https://aws.amazon.com/api/dirs/blog-posts/items?order_by=SortOrderValue&sort_ascending=true&limit=10&locale=en_US
+    const orderBy = 'SortOrderValue';
+    const sortAscending = false;
+    const limit = NUMBER_TO_CHECK;
+    const locale = 'en_US';
+    const awsBlogUrl =
+        `https://aws.amazon.com/api/dirs/blog-posts/items?` +
+        `order_by=${orderBy}` +
+        `&sort_ascending=${sortAscending}` +
+        `&limit=${limit}` +
+        `&locale=${locale}`;
+    // https://aws.amazon.com/api/dirs/blog-posts/items?order_by=SortOrderValue&sort_ascending=true&limit=10&locale=en_US
 
-  axios.get(awsBlogUrl).then((response) => {
-    // success case expression:
-    const blogPosts = response.data;
-    //console.log(JSON.stringify(blogPosts, undefined, 4));
-    var event = [];
-    var urlList = []; // Must be same size or bigger than event array above.
-    var eventNo = 0;
-    var unTweetedUrlCounter = 0;
-    // Work from the oldest back to 0 (the newest)
-    blog_post_items:
-      for (var i = blogPosts.items.length - 1; i >= 0; i--) {
-        var strTime = blogPosts.items[i].dateCreated;
-        strTime = strTime.substr(0, DATE_FORMAT.length);
-        var blogPostTimestamp = new Date(strTime + 'Z');
-        var currentTime = new Date();
-        const PERIOD = 1000 * 60 * MINUTES_IN_PERIOD;
-        const BLOG_POST_AGE = currentTime - blogPostTimestamp;
+    axios.get(awsBlogUrl).then((response) => {
+        // success case expression:
+        const blogPosts = response.data;
+        //console.log(JSON.stringify(blogPosts, undefined, 4));
+        var event = [];
+        var urlList = []; // Must be same size or bigger than event array above.
+        var eventNo = 0;
+        var unTweetedUrlCounter = 0;
+        // Work from the oldest back to 0 (the newest)
+        blog_post_items:
+        for (var i = blogPosts.items.length - 1; i >= 0; i--) {
+            var dateCreated = blogPosts.items[i].dateCreated;
+            dateCreated = dateCreated.substr(0, DATE_FORMAT.length);
+            var blogPostTimestamp = new Date(dateCreated + 'Z');
+            var currentTime = new Date();
+            const PERIOD = 1000 * 60 * MINUTES_IN_PERIOD;
+            const BLOG_POST_AGE = currentTime - blogPostTimestamp;
 
-        var author = JSON.parse(blogPosts.items[i].author);
-        var strUrl = blogPosts.items[i].additionalFields.link;
-        var changeableUrl = strUrl.substr(BLOG_ADDRESS_STUB.length);
-        var sectionName = changeableUrl.substr(0, changeableUrl.indexOf('/'));
+            var author = JSON.parse(blogPosts.items[i].author);
+            var url = blogPosts.items[i].additionalFields.link;
+            var changeableUrl = url.substr(BLOG_ADDRESS_STUB.length);
+            var section = changeableUrl.substr(0, changeableUrl.indexOf('/'));
 
-        recordTweet(blogPosts.items[i], ENV);
-
-        if (BLOG_POST_AGE < PERIOD) {
-          // Only add URLs to be posted, to the list (not the number-to-check)
-          urlList[unTweetedUrlCounter++] = {
-            url: strUrl,
-            section: sectionName,
-            author
-          };
-          for (var x = 0; x < event.length; x++) {
-            // More than one blog post is new - checking for duplicate section names to query only unique names
-            if (event[x].URLSection.S === sectionName) continue blog_post_items;
-          }
-          // Create a list of unique section names
-          event[eventNo] = {
-            'URLSection': {
-              S: sectionName
+            if (BLOG_POST_AGE < PERIOD) {
+                // Only add URLs to be posted, to the list (not the number-to-check)
+                urlList[unTweetedUrlCounter++] = {
+                    id: blogPosts.items[i].id,
+                    slug: blogPosts.items[i].additionalFields.slug,
+                    createdBy: blogPosts.items[i].createdBy,
+                    dateUpdated: blogPosts.items[i].dateUpdated,
+                    dateCreated,
+                    title: blogPosts.items[i].additionalFields.title,
+                    url,
+                    section,
+                    author
+                };
+                for (var x = 0; x < event.length; x++) {
+                    // More than one blog post is new - checking for duplicate section names to query only unique names
+                    if (event[x].URLSection.S === section) continue blog_post_items;
+                }
+                // Create a list of unique section names
+                event[eventNo] = {
+                    'URLSection': {
+                        S: section
+                    }
+                };
+                eventNo++;
             }
-          };
-          eventNo++;
         }
-      }
-    return event.length > 0 ? dynamo.queryDatabase(event, undefined, urlList, ENV) : {
-      statusCode: 200,
-      body: []
-    };
-  }).then(async (resolve) => {
-    if (resolve.statusCode === 200) {
-      for (var item of resolve.body) {
-        // console.log('resolve.ref:', resolve.ref);
-        // console.log('item.section:', item.section);
-        var output =
-          'The AWS ' + resolve.ref[item.section][0] +
-          ' Blog #' + resolve.ref[item.section][1] +
-          ' ' + item.url +
-          await authorsList(item, ENV);
+        return event.length > 0 ? dynamo.getBlogDetails(event, undefined, urlList, ENV) : {
+            statusCode: 200,
+            body: []
+        };
+    }).then(async (resolve) => {
+        if (resolve.statusCode === 200) {
+            for (var item of resolve.body) {
+                // console.log('resolve:', JSON.stringify(resolve, undefined, 4));
+                recordTweet(item, ENV);
+                var output =
+                    'The AWS ' + resolve.ref[item.section][0] +
+                    ' Blog #' + resolve.ref[item.section][1] +
+                    ' ' + item.url +
+                    await authorsList(item, ENV);
 
-        console.log('Tweeting:', output);
-        try {
-          if (TWITTER_ON) {
-            twitter.sendTweet(output, TWITTER_ACCOUNT);
-          }
-        } catch (error) {
-          console.log('Error is: ', error);
+                console.log('Tweeting:', output);
+                try {
+                    if (TWITTER_ON) {
+                        twitter.sendTweet(output, TWITTER_ACCOUNT);
+                    }
+                } catch (error) {
+                    console.log('Error is: ', error);
+                }
+            }
+        } else {
+            console.log('Status Code is: ', resolve.statusCode);
+            console.log('Error Message is: ', resolve.error);
         }
-      }
-    } else {
-      console.log('Status Code is: ', resolve.statusCode);
-      console.log('Error Message is: ', resolve.error);
-    }
-  }).catch((error) => {
-    console.log('Oops! There was an error:', error);
-    callback(error);
-  });
-  callback(undefined, 'No Return Value Needed ;-)');
+    }).catch((error) => {
+        console.log('Oops! There was an error:', error);
+        callback(error);
+    });
+    callback(undefined, 'No Return Value Needed ;-)');
 }
 
 var recordTweet = (tweet, env) => {
-  for (var author of JSON.parse(tweet.author)) {
-    dynamo.recordAuthorTweetLink(tweet.id, author, env);
-  }
+    // console.log('Tweet:', tweet);
+    for (var author of tweet.author) {
+        dynamo.recordAuthorTweetLink(tweet.id, author, env);
+    }
 
-  dynamo.recordTweetVitals(tweet, env);
+    dynamo.recordTweetVitals(tweet, env);
 }
 
 /**
@@ -119,23 +124,26 @@ var recordTweet = (tweet, env) => {
  * to us.
  */
 var authorsList = async (elementData, env) => {
-  const STUB = ' by: ';
-  const SEPARATOR = ' and ';
-  const ABANDON_VALUE = 'publicsector';
+    const STUB = ' by: ';
+    const SEPARATOR = ' and ';
+    const ABANDON_VALUE = 'publicsector';
 
-  var abandonReturn = false;
-  var output = '';
+    var abandonReturn = false;
+    var output = '';
 
-  for (var person of elementData.author) {
-    if (person === ABANDON_VALUE) abandonReturn = true;
-    console.log('Author to check: ', person);
-    await dynamo.handleAuthorName(person, env).then((resolve) => {
-      output += resolve;
-    });
-    if (elementData.author[elementData.author.length - 1] !== person) {
-      output += SEPARATOR;
+    for (var person of elementData.author) {
+        if (person === ABANDON_VALUE) abandonReturn = true;
+
+        await dynamo.handleAuthorName(person, env).then((resolve) => {
+            output += resolve;
+        }).catch((err) => {
+            console.log('Error on authorsList:', elementData);
+            console.log('err:', JSON.stringify(err, undefined, 4));
+        });
+        if (elementData.author[elementData.author.length - 1] !== person) {
+            output += SEPARATOR;
+        }
     }
-  }
 
-  return (abandonReturn ? '' : STUB + output);
+    return (abandonReturn ? '' : STUB + output);
 }
--- dynamodb/index.js
+++ dynamodb/index.js
@@ -1,241 +1,247 @@
 var AWS = require('aws-sdk');
 AWS.config.update({
-  region: 'eu-west-1'
+    region: 'eu-west-1'
 });
 var ddb = new AWS.DynamoDB({
-  apiVersion: '2012-08-10'
+    apiVersion: '2012-08-10'
 });
 
-var queryDatabase = async (event, context, body, env) => {
-  //  console.log('Event is: ', JSON.stringify(event, undefined, 4));
+// Given as inserting an empty string gives problems.
+const DEFAULT_HANDLE = '@';
 
-  var params = {
-    RequestItems: {
-      [`${env}AWS_BLOGS`]: {
-        Keys: event,
-        ProjectionExpression: 'URLSection, BlogSection, Hashtag'
-      }
+var getBlogDetails = async (event, context, body, env) => {
+    //    console.log('Event is: ', JSON.stringify(event, undefined, 4));
+
+    var params = {
+        RequestItems: {
+            [`${env}AWS_BLOGS`]: {
+                Keys: event,
+                ProjectionExpression: 'URLSection, BlogSection, Hashtag'
+            }
+        }
+    };
+
+    try {
+        var data = await ddb.batchGetItem(params).promise();
+        var data = reArrangeEntries(data, env);
+        // Debugging Return: return { statusCode: 200, body: { params, data } };
+        return {
+            statusCode: 200,
+            ref: data,
+            body
+        };
+    } catch (error) {
+        return {
+            statusCode: 400,
+            error: `Could not post: ${error.stack}`
+        };
     }
-  };
-
-  try {
-    var data = await ddb.batchGetItem(params).promise();
-    var data = reArrangeEntries(data, env);
-    // Debugging Return: return { statusCode: 200, body: { params, data } };
-    return {
-      statusCode: 200,
-      ref: data,
-      body
-    };
-  } catch (error) {
-    return {
-      statusCode: 400,
-      error: `Could not post: ${error.stack}`
-    };
-  }
 };
 
 var checkAuthorName = async (authorName, env) => {
-  var params = {
-    TableName: `${env}TWITTER_HANDLES`,
-    ExpressionAttributeValues: {
-      ':s': {
-        S: authorName
-      }
-    },
-    KeyConditionExpression: 'AuthorName = :s',
-    ProjectionExpression: 'TwitterHandle'
-  };
-
-  try {
-    return await ddb.query(params).promise();
-  } catch (error) {
-    console.log('Error is:', error);
-    return {
-      statusCode: 400,
-      error: `Could not post: ${error}`
+    var params = {
+        TableName: `${env}TWITTER_HANDLES`,
+        ExpressionAttributeValues: {
+            ':s': {
+                S: authorName
+            }
+        },
+        KeyConditionExpression: 'AuthorName = :s',
+        ProjectionExpression: 'TwitterHandle'
     };
-  }
+
+    try {
+        return await ddb.query(params).promise();
+    } catch (error) {
+        console.log('Error is:', error);
+        return {
+            statusCode: 400,
+            error: `Could not post: ${error}`
+        };
+    }
 };
 
 // Default initial value for Twitter Handle is blank NOT 'NONE' until it has been searched-for!
 // This does not need to be synch'd once it is done!
 var addNewAuthor = (authorName, env) => {
-  var returnValue = {};
-  var params = {
-    TableName: `${env}TWITTER_HANDLES`,
-    ReturnConsumedCapacity: "TOTAL",
-    Item: {
-      "AuthorName": {
-        S: authorName
-      },
-      "TwitterHandle": {
-        S: ""
-      },
-      "DateAdded": {
-        N: String(new Date().getTime())
-      },
-      "CountPosts": {
-        N: String(1)
-      }
-    }
-  };
-
-  try {
-    returnValue = ddb.putItem(params).promise();
-  } catch (error) {
-    console.log('Error is:', error);
-    returnValue = {
-      statusCode: 400,
-      error: `Could not post: ${error}`
+    var returnValue = {};
+    var params = {
+        TableName: `${env}TWITTER_HANDLES`,
+        ReturnConsumedCapacity: "TOTAL",
+        Item: {
+            "AuthorName": {
+                S: authorName
+            },
+            "TwitterHandle": {
+                S: DEFAULT_HANDLE
+            },
+            "DateAdded": {
+                N: String(new Date().getTime())
+            },
+            "CountPosts": {
+                N: String(1)
+            }
+        }
     };
-  }
 
-  return returnValue;
+    try {
+        returnValue = ddb.putItem(params).promise();
+    } catch (error) {
+        console.log('Error is:', error);
+        returnValue = {
+            statusCode: 400,
+            error: `Could not post: ${error}`
+        };
+    }
+
+    return returnValue;
 }
 
 var recordTweetVitals = async (tweetVitals, env) => {
-  var returnValue = {};
-  var params = {
-    TableName: `${env}BLOG_POSTS`,
-    ReturnConsumedCapacity: "TOTAL",
-    Item: {
-      "ID": {
-        S: tweetVitals.id
-      },
-      "SLUG": {
-        S: tweetVitals.additionalFields.slug
-      },
-      "CreatedBy": {
-        SS: JSON.parse(tweetVitals.createdBy)
-      },
-      "DateUpdated": {
-        N: String(new Date(tweetVitals.dateUpdated).getTime())
-      },
-      "DateCreated": {
-        N: String(new Date(tweetVitals.dateCreated).getTime())
-      },
-      "Link": {
-        S: tweetVitals.additionalFields.link
-      },
-      "Title": {
-        S: tweetVitals.additionalFields.title
-      }
-    }
-  };
+    var returnValue = {};
+    var params = {
+        TableName: `${env}BLOG_POSTS`,
+        ReturnConsumedCapacity: "TOTAL",
+        Item: {
+            "ID": {
+                S: tweetVitals.id
+            },
+            "Slug": {
+                S: tweetVitals.slug
+            },
+            "CreatedBy": {
+                SS: JSON.parse(tweetVitals.createdBy)
+            },
+            "DateUpdated": {
+                N: String(new Date(tweetVitals.dateUpdated).getTime())
+            },
+            "DateCreated": {
+                N: String(new Date(tweetVitals.dateCreated).getTime())
+            },
+            "Link": {
+                S: tweetVitals.url
+            },
+            "Title": {
+                S: tweetVitals.title
+            }
+        }
+    };
 
-  returnValue = ddb.putItem(params).promise().catch((err) => {
-    console.log(undefined === err ? 'Undefined Error!' : err.message);
-  });
+    returnValue = ddb.putItem(params).promise().catch((err) => {
+        console.log(undefined === err ? 'Undefined Error!' : err.message);
+    });
 
-  return returnValue;
+    return returnValue;
 }
 
 // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB.html#batchWriteItem-property
 var recordAuthorTweetLink = async (id, authorName, env) => {
-  var returnValue = {};
-  var params = {
-    TableName: `${env}POST_AUTHORS`,
-    ReturnConsumedCapacity: "TOTAL",
-    Item: {
-      "PostID": {
-        S: id
-      },
-      "AuthorName": {
-        S: authorName
-      }
-    }
-  };
-
-  try {
-    returnValue = ddb.putItem(params).promise();
-  } catch (error) {
-    console.log('Error is:', error);
-    returnValue = {
-      statusCode: 400,
-      error: `Could not post: ${error}`
+    var returnValue = {};
+    var params = {
+        TableName: `${env}POST_AUTHORS`,
+        ReturnConsumedCapacity: "TOTAL",
+        Item: {
+            "PostID": {
+                S: id
+            },
+            "AuthorName": {
+                S: authorName
+            }
+        }
     };
-  }
 
-  return returnValue;
+    try {
+        returnValue = ddb.putItem(params).promise();
+    } catch (error) {
+        console.log('Error is:', error);
+        returnValue = {
+            statusCode: 400,
+            error: `Could not post: ${error}`
+        };
+    }
+
+    return returnValue;
 }
 
 var handleAuthorName = async (authorName, env) => {
-  var returnValue = '';
-  await checkAuthorName(authorName, env).then((data) => {
-    if (data.Count === 1 && data.Items[0].TwitterHandle === undefined) {
-      console.log('Seen before, previously entered in the DB, but not yet checked for a twitter handle.');
-      // TODO: Update DB check count +1
-      returnValue = authorName;
-    } else if (data.Count === 1 && data.Items[0].TwitterHandle.S !== 'NONE') {
-      // check does returnValue.Items[0].TwittterHandle.S, begine with an @-sign and have the correct number of characters.
-      // TODO-Later: validate against Twitter?
-      console.log('Has a twitter and it is:', data.Items[0].TwitterHandle.S);
-      // TODO: Update DB check count +1
-      returnValue = data.Items[0].TwitterHandle.S;
-      if (!isValidTwitterHandle(returnValue)) {
-        returnValue = authorName;
-      }
-    } else if (data.Count === 1 && data.Items[0].TwitterHandle.S === 'NONE') {
-      console.log('Is known to have no Twitter so use full name in tweet');
-      returnValue = authorName;
-    } else if (data.Count === 0) {
-      console.log('Never before seen, so enter it in DB and use full name for now.');
-      var confirmation = addNewAuthor(authorName, env);
-      console.log('Confirmation:', confirmation);
-      returnValue = authorName;
-    } else {
-      console.log('Unknown case: Duplicate names?, None-misspelt, Multiple entries?, other?');
-      returnValue = authorName;
-    }
-  });
+    var returnValue = '';
+    await checkAuthorName(authorName, env).then((data) => {
+        if (data.Count === 1 && data.Items[0].TwitterHandle.S === DEFAULT_HANDLE) {
+            console.log('Seen before, previously entered in the DB, but not yet checked for a twitter handle.');
+            // TODO: Update DB check count +1
+            returnValue = authorName;
+        } else if (data.Count === 1 && data.Items[0].TwitterHandle.S !== 'NONE') {
+            // check does returnValue.Items[0].TwittterHandle.S, begine with an @-sign and have the correct number of characters.
+            // TODO-Later: validate against Twitter?
+            console.log('Has a twitter and it is:', data.Items[0].TwitterHandle.S);
+            // TODO: Update DB check count +1
+            returnValue = data.Items[0].TwitterHandle.S;
+            if (!isValidTwitterHandle(returnValue)) {
+                returnValue = authorName;
+            }
+        } else if (data.Count === 1 && data.Items[0].TwitterHandle.S === 'NONE') {
+            console.log('Is known to have no Twitter so use full name in tweet');
+            returnValue = authorName;
+        } else if (data.Count === 0) {
+            console.log('Never before seen, so enter it in DB and use full name for now.');
+            var confirmation = addNewAuthor(authorName, env);
+            console.log('Confirmation:', confirmation);
+            returnValue = authorName;
+        } else {
+            console.log('Unknown case: Duplicate names?, None-misspelt, Multiple entries?, other?');
+            returnValue = authorName;
+        }
+    }).catch((err) => {
+        console.log('Error on checkAuthorName:', authorName);
+        console.log('err:', JSON.stringify(err, undefined, 4));
+    });
 
-  return returnValue;
+    return returnValue;
 }
 
 var reArrangeEntries = (data, env) => {
-  var resultsArr = data.Responses[`${env}AWS_BLOGS`];
-  var output = {};
-  for (var result of resultsArr) {
-    output[result.URLSection.S] = [result.BlogSection.S, result.Hashtag.S];
-  }
-  return output;
+    var resultsArr = data.Responses[`${env}AWS_BLOGS`];
+    var output = {};
+    for (var result of resultsArr) {
+        output[result.URLSection.S] = [result.BlogSection.S, result.Hashtag.S];
+    }
+    return output;
 };
 
 var isValidTwitterHandle = async handle => {
-  // Usernames containing the words Twitter or Admin cannot be claimed. No account names can contain Twitter or Admin unless they are official Twitter accounts.
-  // Your username cannot be longer than 15 characters. Your name can be longer (50 characters), but usernames are kept shorter for the sake of ease.
-  // A username can only contain alphanumeric characters (letters A-Z, numbers 0-9) with the exception of underscores, as noted above. Check to make sure your desired username doesn't contain any symbols, dashes, or spaces.
+    // Usernames containing the words Twitter or Admin cannot be claimed. No account names can contain Twitter or Admin unless they are official Twitter accounts.
+    // Your username cannot be longer than 15 characters. Your name can be longer (50 characters), but usernames are kept shorter for the sake of ease.
+    // A username can only contain alphanumeric characters (letters A-Z, numbers 0-9) with the exception of underscores, as noted above. Check to make sure your desired username doesn't contain any symbols, dashes, or spaces.
 
-  var valid = true;
-  var REG_EXP = new RegExp('[0-9a-z_]+', 'i');
+    var valid = true;
+    var REG_EXP = new RegExp('[0-9a-z_]+', 'i');
 
-  if (null !== handle) {
-    handle = handle.substr(1);
+    if (null !== handle && DEFAULT_HANDLE !== handle) {
+        handle = handle.substr(1);
 
-    if (handle.length < 1 || handle.length > 15) {
-      valid = false;
-      console.log('Fail validation 1) length', handle);
-    } else if (null === REG_EXP.exec(handle)) {
-      valid = false;
-      console.log('Fail validation 2) reg ex', handle);
-    } else if (REG_EXP.exec(handle)[0] != handle) {
-      valid = false;
-      console.log('Fail validation 3) reg ex', handle);
-    } else if (handle.toLowerCase().indexOf('twitter') !== -1 || handle.toLowerCase().indexOf('admin') !== -1) {
-      valid = false;
-      console.log('Fail validation 3) content', handle);
+        if (handle.length < 1 || handle.length > 15) {
+            valid = false;
+            console.log('Fail validation 1) length', handle);
+        } else if (null === REG_EXP.exec(handle)) {
+            valid = false;
+            console.log('Fail validation 2) reg ex', handle);
+        } else if (REG_EXP.exec(handle)[0] != handle) {
+            valid = false;
+            console.log('Fail validation 3) reg ex', handle);
+        } else if (handle.toLowerCase().indexOf('twitter') !== -1 || handle.toLowerCase().indexOf('admin') !== -1) {
+            valid = false;
+            console.log('Fail validation 3) content', handle);
+        }
+    } else {
+        console.log('Fail validation 0) null');
     }
-  } else {
-    console.log('Fail validation 0) null');
-  }
 
-  return valid;
+    return valid;
 }
 
 module.exports = {
-  queryDatabase,
-  handleAuthorName,
-  recordTweetVitals,
-  recordAuthorTweetLink
+    getBlogDetails,
+    handleAuthorName,
+    recordTweetVitals,
+    recordAuthorTweetLink
 };
